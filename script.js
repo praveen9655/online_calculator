95dd3cb2b31dd9c39a3c3a69a640cede41957fb8
Praveen K <118113479+praveen9655@users.noreply.github.com>
2023-07-09 17:22:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 let ops = document.getElementById('ans');
+let result = '';
 function display(num){
     ops.value += num;
+    if(result != 0){
+        ops.value = result ? `${result}` : '';
+        result ='';
+        ops.value += num;
+    }
 }
 function eql(){
     try{
-        ops.value = ' = ' +eval(ops.value)
+        result = eval(ops.value);
+        ops.value += ' = ' + result;
     }
     catch{
         alert('invaild')
@@ -12,9 +19,9 @@ function eql(){
 }
 function clr(){
     ops.value='';
+    result = '';
+
 }
 function del(){
     ops.value= ops.value.slice(0,-1);
 }
-
-  
